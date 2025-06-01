a5b32700117eb9e69d353367c72085efbcad2f0a
Iyán <me@iyanmv.com>
2025-06-01 12:38:44 +0200

    Release v0.1.1 (#1)
    * Support Python 3.13
    
    * Update README
    
    - Make structure of the repo more clear
    - Separate Build & Install from Documentation
    - Add Citation & Contact section
    
    * Update copyright year
    
    * Add anchor for test_vectors directory
    
    * Bump version to v0.1.1
    
    * Update docs to v0.1.1
    
    * Add build to dev dependencies
    
    * Remove deprecation messages regarding license and add keywords

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "RandExtract"
-copyright = "2024, HSLU"
+copyright = "2025, HSLU"
 author = "Iyán Méndez Veiga"
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,7 +25,7 @@ extensions = [
     "sphinx_tabs.tabs",
     "sphinx_togglebutton",
 ]
-source_suffix = {'.rst': 'restructuredtext'}
+source_suffix = {".rst": "restructuredtext"}
 bibtex_bibfiles = ["references.bib"]
 bibtex_default_style = "unsrt"
 bibtex_reference_style = "label"
